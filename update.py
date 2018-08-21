b724ea29c81c5328493852886d8059cacc415192
moonlightwatch <moonlightwatch@hotmail.com>
2018-08-21 11:38:49 +0800

    .

--- update.py
+++ update.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 
 import os
+import re
 import json
 import pymongo
 
@@ -16,7 +17,53 @@ class ExportPasswordList(object):
         self.collection = self._get_dbcollection(self._config)
         self.passwords = set()
 
-    def get_all_passwords(self) -> list:
+        self._numbers = list()
+        self._letters = list()
+        self._letters_upper = list()
+        self._letter_lower = list()
+        self._num_letters = list()
+        self._num_symbols = list()
+        self._letter_symbols = list()
+        self._num_letter_symbols = list()
+
+    def output(self) -> None:
+        """
+        输出成文件
+        """
+        with open(file="num_letter_symbols.txt", mode="w") as fp:
+            for item in self._num_letter_symbols:
+                fp.write(item)
+                fp.write("\n")
+        with open(file="letter_symbols.txt", mode="w") as fp:
+            for item in self._letter_symbols:
+                fp.write(item)
+                fp.write("\n")
+        with open(file="num_symbols.txt", mode="w") as fp:
+            for item in self._num_symbols:
+                fp.write(item)
+                fp.write("\n")
+        with open(file="num_letters.txt", mode="w") as fp:
+            for item in self._num_letters:
+                fp.write(item)
+                fp.write("\n")
+        with open(file="letter_lower.txt", mode="w") as fp:
+            for item in self._letter_lower:
+                fp.write(item)
+                fp.write("\n")
+        with open(file="numbers.txt", mode="w") as fp:
+            for item in self._numbers:
+                fp.write(item)
+                fp.write("\n")
+        with open(file="letters.txt", mode="w") as fp:
+            for item in self._letters:
+                fp.write(item)
+                fp.write("\n")
+        with open(file="letters_upper.txt", mode="w") as fp:
+            for item in self._letters_upper:
+                fp.write(item)
+                fp.write("\n")
+
+    def load_all_passwords(self) -> None:
         """
         获取数据
         """
@@ -24,13 +71,38 @@ class ExportPasswordList(object):
         i = 1
         while True:
             s = self.collection.find({}).skip(1000*i).limit(1000)
+            pwd_list = list()
             for item in s:
                 self.passwords.add(item["p"])
+                pwd_list.append(item["p"])
             print(f"{len(self.passwords)}/{count}")
+            self._check_passwords(pwd_list)
             if s.count() < 1000:
                 break
             i += 1
-        return self.passwords
+
+    def _check_passwords(self, passwords: list) -> None:
+        """
+        分类
+        """
+        for pwd in passwords:
+            if re.match("^\d+$", pwd):
+                self._numbers.append(pwd)
+            elif re.match("^[A-Za-z]+$"):
+                self._letters.append(pwd)
+                if re.match("^[A-Z]+$"):
+                    self._letters_upper.append(pwd)
+                else re.match("^[a-z]+$"):
+                    self._letter_lower.append(pwd)
+            elif re.match("^[A-Za-z0-9]+$") and re.search("[A-Za-z]+", pwd) != None and re.search("[0-9]+", pwd) != None:
+                self._num_letters.append(pwd)
+            else:
+                if re.search("[0-9]+", pwd) == None:
+                    self._letter_symbols.append(pwd)
+                elif re.search("[A-Za-z]+", pwd) == None:
+                    self._num_symbols.append(pwd)
+                else:
+                    self._num_letter_symbols.append(pwd)
 
     def _get_config(self) -> dict:
         """
@@ -65,7 +137,7 @@ class ExportPasswordList(object):
 
 def main():
     export = ExportPasswordList()
-    export.get_all_passwords()
+    export.load_all_passwords()
     export.close()
 
 
